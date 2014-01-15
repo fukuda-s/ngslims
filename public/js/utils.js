0aebdfaf3351e6b73b7374e146a9706a61860bee
Shiro Fukuda <fukuda-s@genome.rcast.u-tokyo.ac.jp>
2014-01-15 16:00:18 +0900

    1. Add toggleChevron function. 2. Append dataTable. 3. Append tooltips.

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,61 +1,83 @@
-
 var Profile = {
-    check: function (id) {
-        if ($.trim($("#" + id)[0].value) == '') {
-            $("#" + id)[0].focus();
-            $("#" + id + "_alert").show();
+	check : function(id) {
+		if ($.trim($("#" + id)[0].value) == '') {
+			$("#" + id)[0].focus();
+			$("#" + id + "_alert").show();
 
-            return false;
-        };
+			return false;
+		}
+		;
 
-        return true;
-    },
-    validate: function () {
-        if (SignUp.check("name") == false) {
-            return false;
-        }
-        if (SignUp.check("email") == false) {
-            return false;
-        }
-        $("#profileForm")[0].submit();
-    }
+		return true;
+	},
+	validate : function() {
+		if (SignUp.check("name") == false) {
+			return false;
+		}
+		if (SignUp.check("email") == false) {
+			return false;
+		}
+		$("#profileForm")[0].submit();
+	}
 };
 
 var SignUp = {
-    check: function (id) {
-        if ($.trim($("#" + id)[0].value) == '') {
-            $("#" + id)[0].focus();
-            $("#" + id + "_alert").show();
+	check : function(id) {
+		if ($.trim($("#" + id)[0].value) == '') {
+			$("#" + id)[0].focus();
+			$("#" + id + "_alert").show();
 
-            return false;
-        };
+			return false;
+		}
+		;
 
-        return true;
-    },
-    validate: function () {
-        if (SignUp.check("name") == false) {
-            return false;
-        }
-        if (SignUp.check("username") == false) {
-            return false;
-        }
-        if (SignUp.check("email") == false) {
-            return false;
-        }
-        if (SignUp.check("password") == false) {
-            return false;
-        }
-        if ($("#password")[0].value != $("#repeatPassword")[0].value) {
-            $("#repeatPassword")[0].focus();
-            $("#repeatPassword_alert").show();
+		return true;
+	},
+	validate : function() {
+		if (SignUp.check("name") == false) {
+			return false;
+		}
+		if (SignUp.check("username") == false) {
+			return false;
+		}
+		if (SignUp.check("email") == false) {
+			return false;
+		}
+		if (SignUp.check("password") == false) {
+			return false;
+		}
+		if ($("#password")[0].value != $("#repeatPassword")[0].value) {
+			$("#repeatPassword")[0].focus();
+			$("#repeatPassword_alert").show();
 
-            return false;
-        }
-        $("#registerForm")[0].submit();
-    }
+			return false;
+		}
+		$("#registerForm")[0].submit();
+	}
+};
+
+$(document).ready(function() {
+	$("#registerForm .alert").hide();
+	$("div.profile .alert").hide();
+});
+
+function toggleChevron(e) {
+	$(e.target).parent().prev('.panel-heading').find("i.indicator")
+			.toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
 }
+$('#projectOverview').on('hidden.bs.collapse', toggleChevron);
+$('#projectOverview').on('shown.bs.collapse', toggleChevron);
 
-$(document).ready(function () {
-    $("#registerForm .alert").hide();
-    $("div.profile .alert").hide();
+$(function() {
+	$('a[rel=tooltip]').tooltip({
+		container : 'body'
+	});
+});
+
+$(document).ready(function() {
+	$('#sampleInfo_table').dataTable({
+		"sScrollY" : "400px",
+		"bPaginate" : false,
+		"bScrollCollapse" : true
+	});
 });
