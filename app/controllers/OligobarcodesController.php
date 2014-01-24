e8ee20e4a5a96d4d23428b24986b6e50410d637b
Shiro Fukuda <fukuda-s@genome.rcast.u-tokyo.ac.jp>
2014-01-24 14:04:44 +0900

    Addtional commit with previus one.

---
+++ app/controllers/OligobarcodesController.php
@@ -0,0 +1,27 @@
+<?php
+use Phalcon\Logger\Formatter\Json;
+
+class OligobarcodesController extends ControllerBase {
+
+	public function indexAction() {
+		echo "This is index of OligobarcodesController";
+	}
+
+	public function loadjsonAction() {
+		$this->view->disable();
+		$request = new \Phalcon\Http\Request();
+		// Check whether the request was made with method POST
+		if ( $request->isGet() == true ) {
+			// Check whether the request was made with Ajax
+			if ( $request->isAjax() == true ) {
+				// echo "Request was made using POST and AJAX";
+
+				$oligobarcodes = Oligobarcodes::find(array(
+					"active = 'Y'",
+					"order" => "sort_order"
+				));
+				echo json_encode($oligobarcodes->toArray());
+			}
+		}
+	}
+}
