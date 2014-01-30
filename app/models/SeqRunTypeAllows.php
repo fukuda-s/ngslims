a3160843a8ffd86e19b473dd1725647eeb1778c9
Shiro Fukuda <fukuda-s@genome.rcast.u-tokyo.ac.jp>
2014-01-30 15:05:18 +0900

    Depended on changing of schema.  1. Add new table SeqRunreadTypes  2. Change table name SeqRuncycleType to SeqRunType.

---
+++ app/models/SeqRunTypeAllows.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+
+class SeqRunTypeAllows extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $instrument_type_id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $seq_runmode_type_id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $seq_runread_type_id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $seq_runcycle_type_id;
+     
+    /**
+     *
+     * @var string
+     */
+    public $active;
+     
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'instrument_type_id' => 'instrument_type_id', 
+            'seq_runmode_type_id' => 'seq_runmode_type_id', 
+            'seq_runread_type_id' => 'seq_runread_type_id', 
+            'seq_runcycle_type_id' => 'seq_runcycle_type_id', 
+            'active' => 'active'
+        );
+    }
+
+}
---
+++ app/models/SeqRunreadTypes.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+
+class SeqRunreadTypes extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+     
+    /**
+     *
+     * @var string
+     */
+    public $name;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $sort_order;
+     
+    /**
+     *
+     * @var string
+     */
+    public $active;
+     
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'name' => 'name', 
+            'sort_order' => 'sort_order', 
+            'active' => 'active'
+        );
+    }
+
+}
