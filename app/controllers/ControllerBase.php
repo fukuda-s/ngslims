a35c175a03a48316bdaaf538a94b0caf89b4f15d
Shiro Fukuda <fukuda-s@genome.rcast.u-tokyo.ac.jp>
2014-02-07 15:31:36 +0900

    Fix to forward url index/index.

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -11,11 +11,20 @@ class ControllerBase extends Phalcon\Mvc\Controller
     protected function forward($uri)
     {
         $uriParts = explode('/', $uri);
-        return $this->dispatcher->forward(
-            array(
-                'controller' => $uriParts[0],
-                'action' => $uriParts[1]
-            )
-        );
+        if (count($uriParts) > 1) {
+            return $this->dispatcher->forward(
+                array(
+                    'controller' => $uriParts[0],
+                    'action' => $uriParts[1]
+                )
+            );
+        } else {
+            return $this->dispatcher->forward(
+                array(
+                    'controller' => 'index',
+                    'action' => $uriParts[0]
+                )
+            );
+        }
     }
 }
