d4edca4098a53658794ac2c2acccd0eea0a3b114
fukuda-s <fukuda-s@genome.rcast.u-tokyo.ac.jp>
2018-10-18 11:18:40 +0900

    Update handsontable 6.0.1 to 6.1.0.

--- public/js/handsontable-6.0.0/src/cellTypes/checkboxType.js
+++
@@ -1,9 +0,0 @@
-import { getEditor } from '../editors/index';
-import { getRenderer } from '../renderers/index';
-
-const CELL_TYPE = 'checkbox';
-
-export default {
-  editor: getEditor(CELL_TYPE),
-  renderer: getRenderer(CELL_TYPE),
-};
--- public/js/handsontable-6.0.0/src/cellTypes/textType.js
+++
@@ -1,9 +0,0 @@
-import { getEditor } from '../editors/index';
-import { getRenderer } from '../renderers/index';
-
-const CELL_TYPE = 'text';
-
-export default {
-  editor: getEditor(CELL_TYPE),
-  renderer: getRenderer(CELL_TYPE),
-};
